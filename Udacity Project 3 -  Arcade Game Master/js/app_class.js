b965069edc892ce6f6118d860a36d032b48a5773
S1014711679 <32658281+S1014711679@users.noreply.github.com>
2019-08-06 15:18:58 -0300

    Update app_class.js

--- Udacity Project 3 -  Arcade Game Master/js/app_class.js
+++ Udacity Project 3 -  Arcade Game Master/js/app_class.js
@@ -35,10 +35,14 @@ class Enemy {
 
     //check the collsion between enemies and player, then set the player to original location
     checkCollision() {
-        if (Math.abs(this.x - player.x) <= 50 && Math.abs(this.y - player.y) <= 10) {
-            player.x = 202;
-            player.y = 405;
+        if (player.player_x < this.x + 51 &&
+            player.player_x + 51 > this.x &&
+            player.player_y < this.y + 40 &&
+            player.player_y + 40 > this.y) {
+            player.player_x = 202;
+            player.player_y = 405;
         }
+
     }
 
     // Draw the enemy on the screen, required method for game
@@ -54,17 +58,24 @@ class Enemy {
  */
 
 class Player {
-    constructor(x, y) {
+    constructor(player_x, player_y) {
         this.player = 'images/char-boy.png';
-        this.x = x;
-        this.y = y;
+        this.player_x = player_x;
+        this.player_y = player_y;
     }
 
     update() {
+        //if the player get into water-block for 3s, reset the player to the initial location
+        if (this.player_y <= 10) {
+            setTimeout(() => {
+                this.player_x = 202;
+                this.player_y = 405;
+            }, 3000);
+        }
 
     }
     render() {
-        ctx.drawImage(Resources.get(this.player), this.x, this.y);
+        ctx.drawImage(Resources.get(this.player), this.player_x, this.player_y);
     }
 
     /** Player  - Scope and Movements
@@ -73,30 +84,22 @@ class Player {
      */
     handleInput(key) {
         //move left and check the leftmost
-        if (this.x >= 101 && key == "left") {
-            this.x = this.x - 101;
+        if (this.player_x >= 101 && key == "left") {
+            this.player_x = this.player_x - 101;
         }
         //move right and check the right most
-        if (this.x <= 303 && key == "right") {
-            this.x = this.x + 101;
+        if (this.player_x <= 303 && key == "right") {
+            this.player_x = this.player_x + 101;
         }
         //move up and chech the top line
-        if (this.y >= 73 && key == "up") {
-            this.y = this.y - 83;
+        if (this.player_y >= 73 && key == "up") {
+            this.player_y = this.player_y - 83;
         }
         //move down and check the bottom line
-        if (this.y <= 322 && key == 'down') {
-            this.y = this.y + 83;
+        if (this.player_y <= 322 && key == 'down') {
+            this.player_y = this.player_y + 83;
         }
 
-        setTimeout(() => {
-            //if the player get into water-block for 3s, reset the player to the initial location
-
-            if (this.y <= -10) {
-                player.x = 202;
-                player.y = 405;
-            }
-        }, 3000);
     }
 
 }
@@ -111,15 +114,17 @@ const player = new Player(202, 405);
  *  move vertically : 83
  *  move horizontally: based on speed
  */
-const allEnemies = []; // to store all the enmies in array
-const enemyLocations = [63, 146, 229]; // set the start y axis location
 
-//create enemies and push to the allEnemies array and add to the canvas
-enemyLocations.forEach((y) => {
-    const enemy = new Enemy(0, y, 202); // create enemy: initial location (0,y), speed
-    allEnemies.push(enemy); // add enemy to allEnemies array
-});
 
+/** create enemies and push to the allEnemies array 
+ *  and add to the canvas [63, 146, 229]
+ *  set the start y axis location*/
+
+const allEnemies = [
+    new Enemy(0, 63, 202),
+    new Enemy(0, 146, 202),
+    new Enemy(0, 229, 202),
+];
 
 // This listens for key presses and sends the keys to your Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
