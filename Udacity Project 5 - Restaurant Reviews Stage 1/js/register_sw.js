9fb6990883408164b1fd503968007bd4ca966501
Chao Xue <32658281+S1014711679@users.noreply.github.com>
2019-09-02 19:45:11 -0300

    Add files via upload

---
+++ Udacity Project 5 - Restaurant Reviews Stage 1/js/register_sw.js
@@ -0,0 +1,8 @@
+if (navigator.serviceWorker) {
+    navigator.serviceWorker.register('./sw.js')
+        .then(function() {
+            console.log("It succeeded");
+        }).catch(function() {
+            console.log("It failed!");
+        });
+}
